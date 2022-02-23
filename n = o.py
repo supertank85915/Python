8140e5577ababec3a5a0ddefc90eb78c61f1fa7a
supertank85915 <71749498+supertank85915@users.noreply.github.com>
2022-02-23 15:49:14 +0900

    Add files via upload

---
+++ n = o.py
@@ -0,0 +1,9 @@
+n = 0
+
+while True:
+    print(n, end = " ")
+    n = n + 1
+
+    if n == 200:
+        print("200이 되었습니다")
+        continue                                                         #break
