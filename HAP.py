ac15e32499b2fb3906fe13b676453711ba3c98e2
supertank85915 <71749498+supertank85915@users.noreply.github.com>
2022-02-23 15:48:33 +0900

    Add files via upload

---
+++ HAP.py
@@ -0,0 +1,29 @@
+def plus(v1, v2):
+    result = 0
+    result = v1+ v2
+    return result
+hap = plus(100,200)
+print("100과 200의 plus() 함수 결과는 %d"  %hap)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#매개 변수 v1, v2
+#반환값 return result
+# 매개변수 x 반환값 x
+# 매개변수 o 반환값 x
+# 매개변수 x 반환값 o
+# 매개변수 o 반환값 o
