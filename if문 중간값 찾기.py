eaccfdd5b77b5561583f9a0129379462b725f5f0
supertank85915 <71749498+supertank85915@users.noreply.github.com>
2022-02-23 15:42:57 +0900

    Add files via upload

---
+++ if문 중간값 찾기.py
@@ -0,0 +1,19 @@
+a = int(input("첫번째 숫자"))
+b = int(input("두버제 숫자"))
+c = int(input("세번째 숫자"))
+
+if b > a:
+      a < c:
+            print(a)
+      else:
+            print(c)
+if a > b:
+      b < c:
+            print(b)
+      else:
+            print(c)
+if c > a:
+      c < b:
+            print(c)
+      else:
+            print(a)
