d5fe42597f5a6e523be1d40a54b0422935dea0d2
supertank85915 <71749498+supertank85915@users.noreply.github.com>
2022-02-23 15:43:59 +0900

    Add files via upload

---
+++ if문 에제 1.py
@@ -0,0 +1,8 @@
+a = int(input("입력:"))
+
+if 0 < a and a <= 10:
+      print("입력값은 10보다 작습니다")
+elif 10 < a and a<50:
+      print("10보다 크고, 50보다 작다")
+elif 50 < a and a <= 100:
+      print("51보다 크고, 100보다 작다")
